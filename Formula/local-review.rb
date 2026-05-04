8ec6519773ef4073d2ae8b3f215b93b96cdaf446
Maksym Shykov <maksym.shykov@gmail.com>
2026-05-04 11:49:28 +0100

    Update SHA256 checksums for v0.1.1

--- Formula/local-review.rb
+++ Formula/local-review.rb
@@ -7,20 +7,20 @@ class LocalReview < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mshykov/local-review/releases/download/v#{version}/local-review_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+      sha256 "aaf384aef0aefacd75ab23934ba02b325dc5aff5782b76ad547678a9b41f8e1c"
     else
       url "https://github.com/mshykov/local-review/releases/download/v#{version}/local-review_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+      sha256 "a20e4f0440cccd53fb886caa794001c185bf23ad311ae5b52c7a10d602656830"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/mshykov/local-review/releases/download/v#{version}/local-review_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "c8aa0d16d854a18873bb05d8e1cf36bdeaa96dc6f7e72e8849ef382306c9c183"
     else
       url "https://github.com/mshykov/local-review/releases/download/v#{version}/local-review_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      sha256 "5a26566ea3b7db8d7b6446d44e7062c668ae72e142e4cefef9acdab15fe2f471"
     end
   end
 
